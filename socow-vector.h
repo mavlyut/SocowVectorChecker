425d09b1e4a0976e25f71f8282346da8a24eee57
mavlyut <334918@niuitmo.ru>
2022-05-22 21:00:28 +0300

    16

--- socow-vector.h
+++ socow-vector.h
@@ -6,40 +6,39 @@ struct socow_vector {
     using iterator = T*;
     using const_iterator = T const*;
 
-    socow_vector() : size_(0), small(true)
-    {}
+    socow_vector() : _size(0), is_small(true) {}
 
-    socow_vector(socow_vector const& other) :
-          size_(other.size_), small(other.small) {
-        if (other.small) {
-            copy_range(const_cast<T*>(other.static_storage), static_storage, other.size_);
+    socow_vector(socow_vector const& other) : _size(other._size), is_small(other.is_small) {
+        if (other.is_small) {
+            copy(const_cast<T*>(other.small_storage), small_storage, other._size);
         } else {
-            new(&dynamic_storage) dynamic_buffer(other.dynamic_storage);
+            new(&big_storage) storage(other.big_storage);
         }
     }
 
     socow_vector& operator=(socow_vector const& other) {
-        if (this != &other) {
-            socow_vector t(other);
-            t.swap(*this);
+        if (&other != this) {
+            socow_vector(other).swap(*this);
         }
         return *this;
     }
 
+    // todo: fix
     ~socow_vector() {
-        if (small) {
-            destruct_range(current_begin(), current_end());
-        } else {
-            if (dynamic_storage.unique()) {
-                destruct_range(current_begin(), current_end());
-            }
-            dynamic_storage.~dynamic_buffer();
+        if (is_small) {
+            remove(my_begin(), my_end());
+            return;
+        }
+        if (big_storage.is_unique()) {
+            remove(my_begin(), my_end());
+            big_storage.~storage();
         }
     }
 
     T& operator[](size_t i) {
         return *(begin() + i);
     }
+
     T const& operator[](size_t i) const {
         return *(begin() + i);
     }
@@ -47,17 +46,19 @@ struct socow_vector {
     T* data() {
         return begin();
     }
+
     T const* data() const {
         return begin();
     }
 
     size_t size() const {
-        return size_;
+        return _size;
     }
 
     T& front() {
         return *begin();
     }
+
     T const& front() const {
         return *begin();
     }
@@ -65,120 +66,123 @@ struct socow_vector {
     T& back() {
         return *(end() - 1);
     }
+
     T const& back() const {
         return *(end() - 1);
     }
 
-    void push_back(T const& t) {
-        if (capacity() == size_) {
-            T t_tmp = t;
-            extend_buffer(capacity() * 2, begin(), size_);
-            new (begin() + size_) T(t_tmp);
+    void push_back(T const& element) {
+        if (_size == capacity()) {
+            T tmp = element;
+            expand_storage(begin(), capacity() * 2);
+            new(begin() + _size) T(tmp);
         } else {
-            new (begin() + size_) T(t);
+            new(begin() + _size) T(element);
         }
-        size_++;
+        ++_size;
     }
+
     void pop_back() {
         (end() - 1)->~T();
-        size_--;
+        --_size;
     }
 
     bool empty() const {
-        return size_ == 0;
+        return _size == 0;
     }
 
     size_t capacity() const {
-        if (small) {
-            return SMALL_SIZE;
-        }
-        return dynamic_storage.m_data->capacity_;
+        return is_small ? SMALL_SIZE : big_storage.capacity();
     }
-    void reserve(size_t new_cap) {
-        if ((small && new_cap > SMALL_SIZE) || (!small && new_cap > capacity())) {
-            extend_buffer(new_cap, current_begin(), size_);
-        } else if (!small && !dynamic_storage.unique()) {
-            extend_buffer(capacity(), current_begin(), size_);
+
+    void reserve(size_t new_capacity) {
+        if ((is_small && new_capacity > SMALL_SIZE) || (!is_small && new_capacity > capacity())) {
+            expand_storage(my_begin(), new_capacity);
+        } else if (!is_small && !big_storage.is_unique()) {
+            expand_storage(my_begin(), capacity());
         }
     }
+
     void shrink_to_fit() {
-        if (!small) {
-            if (size_ <= SMALL_SIZE) {
-                dynamic_buffer tmp = dynamic_storage;
-                dynamic_storage.~dynamic_buffer();
-                try {
-                    copy_range(tmp.get(), static_storage, size_);
-                } catch (...) {
-                    new(&dynamic_storage) dynamic_buffer(tmp);
-                    throw;
-                }
-                destruct_range(tmp.get(), tmp.get() + size_);
-                small = true;
-            } else if (size_ != capacity()) {
-                extend_buffer(size_, dynamic_storage.get(), size_);
+        if (is_small) return;
+        if (_size <= SMALL_SIZE) {
+            storage tmp = big_storage;
+            big_storage.~storage();
+            try {
+                copy(tmp._data(), small_storage, _size);
+            } catch (...) {
+                new(&big_storage) storage(tmp);
+                throw;
             }
+            remove(tmp._data(), tmp._data() + _size);
+            is_small = true;
+        } else if (_size != capacity()) {
+            expand_storage(big_storage._data(), _size);
         }
     }
 
     void clear() {
-        if (small || dynamic_storage.unique()) {
-            destruct_range(current_begin(), current_end());
-        } else {
-            dynamic_storage.~dynamic_buffer();
-            new(&dynamic_storage) dynamic_buffer(capacity());
-        }
-        size_ = 0;
+        erase(begin(), end());
     }
 
     void swap(socow_vector& other) {
-        if (!small && !other.small) {
-            std::swap(dynamic_storage, other.dynamic_storage);
-        } else if (small  && other.small) {
-            this->swap_small(other);
-        } else if (small  && !other.small) {
-            this->swap_to_big(other);
-        } else {
-            other.swap_to_big(*this);
+        if (_size > other._size || (!is_small && other.is_small)) {
+            other.swap(*this);
+            return;
         }
-        std::swap(other.size_, size_);
-        std::swap(other.small, small);
+        if (is_small && other.is_small) {
+            for (size_t i = 0; i < _size; ++i) {
+                std::swap(small_storage[i], other.small_storage[i]);
+            }
+            for (size_t i = _size; i < other._size; ++i) {
+                new(small_storage + i) T(other.small_storage[i]);
+                other.small_storage[i].~T();
+            }
+        } else if (!is_small && !other.is_small) {
+            std::swap(big_storage, other.big_storage);
+        } else {
+            storage tmp = other.big_storage;
+            other.big_storage.~storage();
+            try {
+                copy(small_storage, other.small_storage, _size);
+            } catch (...) {
+                new(&other.big_storage) storage(tmp);
+                throw;
+            }
+            remove(my_begin(), my_end());
+            new(&big_storage) storage(tmp);
+        }
+        std::swap(_size, other._size);
+        std::swap(is_small, other.is_small);
     }
 
     iterator begin() {
-        if (small) {
-            return static_storage;
-        }
-        create_new_copy();
-        return dynamic_storage.get();
+        if (is_small) return small_storage;
+        make_copy();
+        return big_storage._data();
     }
+
     iterator end() {
-        if (small) {
-            return static_storage + size_;
-        }
-        create_new_copy();
-        return dynamic_storage.get() + size_;
+        if (is_small) return small_storage + _size;
+        make_copy();
+        return big_storage._data() + _size;
     }
 
     const_iterator begin() const {
-        if (small) {
-            return static_storage;
-        }
-        return dynamic_storage.get();
+        return is_small ? small_storage : big_storage._data();
     }
+
     const_iterator end() const {
-        if (small) {
-            return static_storage + size_;
-        }
-        return dynamic_storage.get() + size_;
+        return (is_small ? small_storage : big_storage._data()) + _size;
     }
 
     iterator insert(const_iterator pos, T const& t) {
-        size_t p = pos - current_begin();
+        ptrdiff_t diff = pos - begin();
         push_back(t);
-        for (size_t i = size_ - 1; i > p; i--) {
-            std::swap(*(current_begin() + i), *(current_begin() + i - 1));
+        for (size_t i = _size - 1; i > diff; --i) {
+            std::swap(*(my_begin() + i), *(my_begin() + i - 1));
         }
-        return current_begin() + p;
+        return my_begin() + diff;
     }
 
     iterator erase(const_iterator pos) {
@@ -186,168 +190,141 @@ struct socow_vector {
     }
 
     iterator erase(const_iterator first, const_iterator last) {
-        size_t pos = last - current_begin();
-        size_t res = first - current_begin();
-        create_new_copy();
-        for (auto it = current_begin() + pos; it < current_end(); it++) {
-            std::swap(*it, *(it - (last - first)));
+        ptrdiff_t count = last - first;
+        ptrdiff_t start = first - begin();
+        for (size_t i = start; i < _size - count; i++) {
+            std::swap(operator[](i), operator[](i + count));
         }
-        auto old_size = size_;
-        size_ -= last - first;
-        destruct_range(current_begin() + size_, current_begin() + old_size);
-        return current_begin() + res;
+        for (size_t i = 0; i < count; ++i) {
+            pop_back();
+        }
+        return begin() + start;
     }
+
+    bool is_small;
 
 private:
-    void create_new_copy() {
-        if (!small && !dynamic_storage.unique()) {
-            extend_buffer(capacity(), dynamic_storage.get(), size_);
+    iterator my_begin() {
+        return is_small ? small_storage : big_storage._data();
+    }
+
+    iterator my_end() {
+        return my_begin() + _size;
+    }
+
+    void make_copy() {
+        if (!is_small && !big_storage.is_unique()) {
+            expand_storage(big_storage._data(), capacity());
         }
     }
 
-    void extend_buffer(size_t new_capacity, T const* source, size_t count) {
+    void copy(T const* from, T* to, size_t count) {
+        size_t i = 0;
+        try {
+            while (i < count) {
+                new(to + i) T(from[i]);
+                ++i;
+            }
+        } catch (...) {
+            remove(to, to + i);
+            throw;
+        }
+    }
+
+    void remove(T* start, T* end) {
+        if (start != nullptr) {
+            ptrdiff_t count = end - start;
+            for (ptrdiff_t i = count - 1; i >= 0; --i) {
+                (start + i)->~T();
+            }
+        }
+    }
+
+    void expand_storage(T const* from, size_t new_capacity) {
         if (new_capacity == 0) {
-            size_ = 0;
+            _size = 0;
             return;
         }
-        dynamic_buffer tmp(new_capacity);
-        copy_range(source, tmp.get(), count);
-        if (small || dynamic_storage.unique()) {
-            destruct_range(current_begin(), current_end());
+        storage tmp(new_capacity);
+        copy(from, tmp._data(), _size);
+        if (is_small || big_storage.is_unique()) {
+            remove(my_begin(), my_end());
         }
-        if (!small) {
-            dynamic_storage.~dynamic_buffer();
+        if (!is_small) {
+            big_storage.~storage();
         }
-        new (&dynamic_storage) dynamic_buffer(tmp);
-        size_ = count;
-        small = false;
+        new(&big_storage) storage(tmp);
+        is_small = false;
     }
 
-    void swap_to_big(socow_vector& big) {
-        dynamic_buffer tmp = big.dynamic_storage;
-        big.dynamic_storage.~dynamic_buffer();
-        try {
-            copy_range(static_storage, big.static_storage, size_);
-        } catch (...) {
-            new(&big.dynamic_storage) dynamic_buffer(tmp);
-            throw;
-        }
-        destruct_range(current_begin(), current_end());
-        new(&dynamic_storage) dynamic_buffer(tmp);
-    }
-
-    void swap_small(socow_vector& b) {
-        size_t i = 0;
-        while (i < size_ && i < b.size_) {
-            std::swap(static_storage[i], b.static_storage[i]);
-            i++;
-        }
-        while (i < size_) {
-            new (b.static_storage + i) T(static_storage[i]);
-            static_storage[i].~T();
-            i++;
-        }
-        while (i < b.size_) {
-            new (static_storage + i) T(b.static_storage[i]);
-            b.static_storage[i].~T();
-            i++;
-        }
-    }
-
-    iterator current_begin() {
-        if (small) {
-            return static_storage;
-        }
-        return dynamic_storage.get();
-    }
-    iterator current_end() {
-        if (small) {
-            return static_storage + size_;
-        }
-        return dynamic_storage.get() + size_;
-    }
-
-    void copy_range(T const* source, T* receiver, size_t count) {
-        size_t i = 0;
-        try {
-            while (i != count) {
-                new (receiver + i) T(source[i]);
-                i++;
-            }
-        } catch (...) {
-            destruct_range(receiver, receiver + i);
-            throw;
-        }
-    }
-
-    static void destruct_range(T* start, T* end) {
-        if (start != nullptr) {
-            for (auto it = --end; it >= start; it--) {
-                it->~T();
-            }
-        }
-    }
-
-    struct metadata {
-        size_t capacity_;
-        size_t ref_counter;
-        T data_[];
+    struct control_block {
+        size_t _counter;
+        size_t _capacity;
+        T _data[0];
     };
 
-    struct dynamic_buffer {
-        metadata* m_data;
+    struct storage {
+        storage() = default;
 
-        explicit dynamic_buffer(size_t cap) : m_data(static_cast<metadata*>(operator new(sizeof(metadata) + sizeof(T) * cap,
-                                            static_cast<std::align_val_t>(alignof(metadata))))) {
-            new(&m_data->ref_counter) size_t(1);
-            new(&m_data->capacity_) size_t(cap);
+        explicit storage(size_t capacity) :
+            ctrl(static_cast<control_block*>(operator new(sizeof(control_block) + sizeof(T) * capacity,
+                 static_cast<std::align_val_t>(alignof(control_block))))) {
+            new(&ctrl->_counter) size_t(1);
+            new(&ctrl->_capacity) size_t(capacity);
         }
 
-        dynamic_buffer(dynamic_buffer const& other) : m_data(other.m_data) {
-            m_data->ref_counter++;
+        storage(storage const& other) : ctrl(other.ctrl) {
+            ctrl->_counter++;
         }
 
-        ~dynamic_buffer() {
-            if (m_data->ref_counter == 1) {
-                operator delete(m_data, static_cast<std::align_val_t>(alignof(metadata)));
-            } else {
-                m_data->ref_counter--;
-            }
-        }
-
-        dynamic_buffer& operator=(dynamic_buffer const& other) {
+        storage& operator=(storage const& other) {
             if (&other != this) {
-                dynamic_buffer tmp(other);
-                std::swap(tmp.m_data, this->m_data);
+                storage tmp(other);
+                std::swap(tmp.ctrl, this->ctrl);
             }
             return *this;
         }
 
-        T& operator[](size_t index) {
-            return m_data->data_[index];
+        ~storage() {
+            if (ctrl->_counter == 1) {
+                operator delete(ctrl, static_cast<std::align_val_t>(alignof(ctrl)));
+            } else {
+                ctrl->_counter--;
+            }
         }
 
-        T& operator[](size_t index) const {
-            return m_data->data_[index];
+        T& operator[](size_t i) {
+            return ctrl->_data[i];
         }
 
-        size_t unique() {
-            return m_data->ref_counter == 1;
+        T& operator[](size_t i) const {
+            return ctrl->_data[i];
         }
 
-        T* get() {
-            return m_data->data_;
+        bool is_unique() {
+            return ctrl->_counter == 1;
         }
 
-        T* get() const {
-            return m_data->data_;
+        T* _data() {
+            return ctrl->_data;
         }
+
+        T* _data() const {
+            return ctrl->_data;
+        }
+
+        size_t capacity() const {
+            return ctrl->_capacity;
+        }
+
+    private:
+        control_block* ctrl;
     };
 
-    size_t size_;
-    bool small;
+    size_t _size;
     union {
-        dynamic_buffer dynamic_storage;
-        T static_storage[SMALL_SIZE];
+        T small_storage[SMALL_SIZE];
+        storage big_storage;
     };
 };
+
