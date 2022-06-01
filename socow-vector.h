97f06c473cf74833ccdcf34a0df54fd4e00a8a30
mavlyut <334918@niuitmo.ru>
2022-06-02 01:21:33 +0300

    55

--- socow-vector.h
+++ socow-vector.h
@@ -142,7 +142,7 @@ struct socow_vector {
         std::swap(small_storage[i], other.small_storage[i]);
       }
       copy(small_storage, other.small_storage, other._size, _size);
-      remove(begin() + other._size, begin() + _size);
+      remove(my_begin() + other._size, my_begin() + _size);
     } else if (!is_small && !other.is_small) {
       std::swap(big_storage, other.big_storage);
     } else {
