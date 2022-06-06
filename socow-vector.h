98c1557f67e3fa3bc3cc2a54c238ef3d5e00c829
mavlyut <334918@niuitmo.ru>
2022-06-06 18:04:50 +0300

    75

--- socow-vector.h
+++ socow-vector.h
@@ -101,8 +101,9 @@ struct socow_vector {
   }
 
   void reserve(size_t new_capacity) {
+    if (new_capacity <= capacity()) return;
     if ((is_small && new_capacity > SMALL_SIZE) || !is_small) {
-      expand_storage(my_begin(), std::max<size_t>(new_capacity, capacity()));
+      expand_storage(my_begin(), new_capacity);
     }
   }
 
